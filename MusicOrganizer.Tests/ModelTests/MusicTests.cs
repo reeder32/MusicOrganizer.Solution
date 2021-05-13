aca50d8c5464e32f6e55e3eb6c5cabafb7ae8b22
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-13 10:31:17 -0700

    :tada: setup files
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

---
+++ MusicOrganizer.Tests/ModelTests/MusicTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MusicOrganizer.Models;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class MusicTests
+  {
+    [TestMethod]
+
+  }
+}
