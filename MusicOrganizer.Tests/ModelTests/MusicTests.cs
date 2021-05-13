718220aaf6581d463f0eb9e818429dd5b7e84267
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-13 10:53:00 -0700

    :white_check_mark: add passing tests for creation of object and object with title
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- MusicOrganizer.Tests/ModelTests/MusicTests.cs
+++ MusicOrganizer.Tests/ModelTests/MusicTests.cs
@@ -9,6 +9,27 @@ namespace MusicOrganizer.Tests
   public class MusicTests
   {
     [TestMethod]
+    public void Music_CreatesInstanceOfMusicObject_Music()
+    {
+      string newTitle = "Purple Rain";
+      Music newRecord = new Music(newTitle);
+      Assert.AreEqual(typeof(Music), newRecord.GetType());
+    }
 
+    [TestMethod]
+    public void Music_AssignTitleToMusicObject_String()
+    {
+      string newTitle = "Purple Rain";
+      Music newRecord = new Music(newTitle);
+      Assert.AreEqual(newTitle, newRecord.Title);
+    }
   }
 }
+
+// Test boilerplate
+// [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+// }
--- MusicOrganizer/Models/Music.cs
+++ MusicOrganizer/Models/Music.cs
@@ -1,4 +1,12 @@
 namespace MusicOrganizer.Models
 {
+  public class Music
+  {
+    public string Title { get; set; }
 
+    public Music(string title)
+    {
+      Title = title;
+    }
+  }
 }
